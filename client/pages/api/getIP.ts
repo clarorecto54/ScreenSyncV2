a1f5bed03f489e8ebbdbb1c15196cd8baf11a4e7
clarorecto54 <clarorecto54@gmail.com>
2025-08-23 21:21:29 +0800

    Fix IP detector

--- client/pages/api/getIP.ts
+++ client/pages/api/getIP.ts
@@ -1,14 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import { networkInterfaces } from "os"
 
 const os = require("os")
 export default function Handler(req: NextApiRequest, res: NextApiResponse) {
     var IP: string = "127.0.0.1" //? Default IP
     try {
-        for (var index = 0; index < 4; index++) { //? Find a valid IP
-            const data: string = os.networkInterfaces()[Object.keys(os.networkInterfaces())[index]][0].address
-            if ((data.split(".").length - 1) === 3) {
-                IP = data //? If valid IP found it will return it
-                break
+        const networks = networkInterfaces()
+        for (const [name, descs] of Object.entries(networks))
+        {
+            if (!descs) continue
+            for (const desc of descs)
+            {
+                if (desc.family === "IPv4" && !desc.internal) IP = desc.address
             }
         }
     } catch (error) { console.log("No LAN Detected running on localhost") }
