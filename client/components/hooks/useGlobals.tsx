ebc6e34ad80d973f9b941134c3262c6e026b5261
clarorecto54 <clarorecto54@gmail.com>
2025-09-05 01:48:49 +0800

    Clean up Global Context

--- client/components/hooks/useGlobals.tsx
+++ client/components/hooks/useGlobals.tsx
@@ -7,20 +7,16 @@ import { ReactNode, createContext, useContext, useEffect, useRef, useState } fro
 import { Socket, io } from "socket.io-client";
 /* -------- INTERFACE ------- */
 const defaultValues: GlobalProps = {
-    socket: null,
-    peer: null,
+    socket: {} as Socket,
+    peer: {} as Peer,
     myInfo: { id: "", IPv4: "", name: "" },
-    IPv4: "",
     meetingCode: "",
     systemPopup: null,
     roomList: [],
     rawWhitelist: "",
     whitelist: [],
     /* -------------------------- */
-    setsocket: () => { },
-    setpeer: () => { },
     setmyInfo: () => { },
-    setIPv4: () => { },
     setmeetingCode: () => { },
     setsystemPopup: () => { },
     setRawWhitelist: () => { },
@@ -30,153 +26,104 @@ const context = createContext<GlobalProps>(defaultValues)
 /* ------ HOOK PROVIDER ----- */
 export function useGlobals() { return useContext<GlobalProps>(context) }
 /* --------- CONTEXT -------- */
-export function GlobalContextProvider({ children }: { children: ReactNode }) {
+export function GlobalContextProvider({ children }: { children: ReactNode })
+{
     /* ----- STATES & HOOKS ----- */
-    const [socket, setsocket] = useState<Socket | null>(null)
-    const [peer, setpeer] = useState<Peer | null>(null)
+    const [socket, setsocket] = useState<Socket>({} as Socket)
+    const [peer, setpeer] = useState<Peer>({} as Peer)
     const [myInfo, setmyInfo] = useState<UserProps>({ id: "", IPv4: "", name: "" })
-    const [IPv4, setIPv4] = useState<string>("")
     const [meetingCode, setmeetingCode] = useState<string>("")
     const [systemPopup, setsystemPopup] = useState<SystemPopupProps | null>(null)
     const [roomList, setRoomList] = useState<RoomProps[]>([])
     const [rawWhitelist, setRawWhitelist] = useState<string>("")
     const [whitelist, setWhitelist] = useState<string[]>([])
     const myInfoRef = useRef<UserProps>(myInfo)
-    /* ------- IP HANDLER ------- */
-    useEffect(() => {
-        if (!IPv4) {
-            fetch("/api/getIP", { cache: "no-store" })
-                .then(res => res.text())
-                .then(IP => {
-                    setIPv4(IP)
-                })
-        } else {
-            setsocket(io(`https://${IPv4}:3001`, {
-                transports: ["websocket", "polling"]
-            }))
-        }
-    }, [IPv4])
-    /* ----- SOCKET HANDLER ----- */
-    useEffect(() => {
-        if (socket) {
-            //* EMIT (REQ)
-            socket.emit("my-ipv4")
-            //* ON (RES)
-            socket.on("accept-req", (targetRoom: string) => {
-                console.log(myInfoRef.current)
-                setsystemPopup(null) //? Close popup
-                socket.emit("join-room", targetRoom, myInfoRef.current) //? Join meeting
-            })
-            socket.on("existing-req", () => setsystemPopup({
-                type: "ERROR",
-                message: "Your username is already used in this room or someone in the pending list.",
-                icon: require("@/public/images/Participants.svg"),
-            }))
-            socket.on("cancel-req", () => setsystemPopup({
-                type: "ERROR",
-                message: "The host did not allow you to join.",
-                icon: require("@/public/images/Close 2.svg")
-            }))
-            socket.on("alert", () => setsystemPopup({
-                type: "ALERT",
-                message: "Press close if you're there.",
-                icon: require("@/public/images/Alert.svg")
-            }))
-            socket.on("user-existed", () => setsystemPopup({
-                type: "ERROR",
-                message: "Username is already existing on the room. Please change your username.",
-                icon: require("@/public/images/Participants.svg"),
-            }))
-            socket.on("join-room", (targetRoom: string) => {
-                setmeetingCode(targetRoom)
-            })
-            socket.on("room-list", (rooms: RoomProps[]) => setRoomList(rooms))
-            socket.on("my-ipv4", (myIPv4: string) => setmyInfo(prev => ({ ...prev, IPv4: myIPv4 })))
-            socket.on("connect", () => {
-                if (typeof window !== "undefined") {
-                    setmyInfo(prev => ({ ...prev, id: socket.id }))
-                    setpeer(new Peer(socket.id, {
-                        host: IPv4,
-                        port: 3002,
-                        path: "/",
-                        secure: true,
-                        pingInterval: 1000,
-                        config: {
-                            bundlePolicy: "max-bundle",
-                            iceCandidatePoolSize: 32,
-                            iceServers: [{ urls: ["stun:192.168.2.49:3003", "stun:192.168.2.49:3004", "stun:192.168.2.49:3005"] }],
-                            iceTransportPolicy: "all",
-                            rtcpMuxPolicy: "require",
-                            sdpSemantics: 'unified-plan'
-                        }
-                    }))
-                }
-            })
-            socket.on("disconnect", () => setmyInfo(prev => ({ ...prev, id: "" })))
-        }
-        return () => {
-            if (socket) {
-                socket.off("accept-req", (targetRoom: string) => {
-                    console.log(myInfoRef.current)
-                    setsystemPopup(null) //? Close popup
-                    socket.emit("join-room", targetRoom, myInfoRef.current) //? Join meeting
-                })
-                socket.off("existing-req", () => setsystemPopup({
-                    type: "ERROR",
-                    message: "Your username is already used in this room or someone in the pending list.",
-                    icon: require("@/public/images/Participants.svg"),
-                }))
-                socket.off("cancel-req", () => setsystemPopup({
-                    type: "ERROR",
-                    message: "The host did not allow you to join.",
-                    icon: require("@/public/images/Close 2.svg")
-                }))
-                socket.off("alert", () => setsystemPopup({
-                    type: "ALERT",
-                    message: "Press close if you're there.",
-                    icon: require("@/public/images/Alert.svg")
-                }))
-                socket.off("user-existed", () => setsystemPopup({
-                    type: "ERROR",
-                    message: "Username is already existing on the room. Please change your username.",
-                    icon: require("@/public/images/Participants.svg"),
-                }))
-                socket.off("join-room", (targetRoom: string) => {
-                    setmeetingCode(targetRoom)
-                })
-                socket.off("room-list", (rooms: RoomProps[]) => setRoomList(rooms))
-                socket.off("my-ipv4", (myIPv4: string) => setmyInfo(prev => ({ ...prev, IPv4: myIPv4 })))
-                socket.off("connect", () => {
-                    if (typeof window !== "undefined") {
-                        setmyInfo(prev => ({ ...prev, id: socket.id }))
-                        setpeer(new Peer(socket.id, {
-                            host: IPv4,
-                            port: 3002,
-                            path: "/",
-                            secure: true,
-                            pingInterval: 1000,
-                            config: {
-                                bundlePolicy: "max-bundle",
-                                iceCandidatePoolSize: 32,
-                                iceServers: [{ urls: ["stun:192.168.2.49:3003", "stun:192.168.2.49:3004", "stun:192.168.2.49:3005"] }],
-                                iceTransportPolicy: "all",
-                                rtcpMuxPolicy: "require",
-                                sdpSemantics: 'unified-plan'
-                            }
-                        }))
-                    }
-                })
-                socket.off("disconnect", () => setmyInfo(prev => ({ ...prev, id: "" })))
+    /* ====================================== */
+    function ConnectPeer(socketId: string)
+    {
+        return new Peer(socketId, {
+            host: window.location.hostname,
+            port: 3002,
+            path: "/",
+            secure: true,
+            pingInterval: 1000,
+            config: {
+                bundlePolicy: "max-bundle",
+                iceCandidatePoolSize: 32,
+                iceServers: [{ urls: [`stun:${window.location.hostname}:3003`, `stun:${window.location.hostname}:3004`, `stun:${window.location.hostname}:3005`] }],
+                iceTransportPolicy: "all",
+                rtcpMuxPolicy: "require",
+                sdpSemantics: 'unified-plan'
             }
+        })
+    }
+    /* ====================================== */
+    useEffect(() =>
+    {
+        const socketInstance = io(`https://${window.location.hostname}:3001`)
+        function AcceptReq(targetRoom: string)
+        {
+            console.log(myInfoRef.current)
+            setsystemPopup(null) //? Close popup
+            socket.emit("join-room", targetRoom, myInfoRef.current) //? Join meeting
         }
-    }, [socket])
+        socketInstance.on("accept-req", AcceptReq)
+        const ExistingReq = () => setsystemPopup({
+            type: "ERROR",
+            message: "Your username is already used in this room or someone in the pending list.",
+            icon: require("@/public/images/Participants.svg"),
+        })
+        socketInstance.on("existing-req", ExistingReq)
+        const CancelReq = () => setsystemPopup({
+            type: "ERROR",
+            message: "The host did not allow you to join.",
+            icon: require("@/public/images/Close 2.svg")
+        })
+        socketInstance.on("cancel-req", CancelReq)
+        const Alert = () => setsystemPopup({
+            type: "ALERT",
+            message: "Press close if you're there.",
+            icon: require("@/public/images/Alert.svg")
+        })
+        socketInstance.on("alert", Alert)
+        const UserExisted = () => setsystemPopup({
+            type: "ERROR",
+            message: "Username is already existing on the room. Please change your username.",
+            icon: require("@/public/images/Participants.svg"),
+        })
+        socketInstance.on("user-existed", UserExisted)
+        const JoinRoom = (targetRoom: string) => setmeetingCode(targetRoom)
+        socketInstance.on("join-room", JoinRoom)
+        const RoomList = (rooms: RoomProps[]) => setRoomList(rooms)
+        socketInstance.on("room-list", RoomList)
+        const OnSocketConnect = () =>
+        {
+            setmyInfo(prev => ({ ...prev, id: socketInstance.id, IPv4: window.location.hostname }))
+            setpeer(ConnectPeer(socketInstance.id))
+        }
+        socketInstance.on("connect", OnSocketConnect)
+        const OnSocketDisconnect = () => setmyInfo(prev => ({ ...prev, id: "" }))
+        socketInstance.on("disconnect", OnSocketDisconnect)
+        setsocket(socketInstance)
+        return () =>
+        {
+            socketInstance.off("accept-req", AcceptReq)
+            socketInstance.off("existing-req", ExistingReq)
+            socketInstance.off("cancel-req", CancelReq)
+            socketInstance.off("alert", Alert)
+            socketInstance.off("user-existed", UserExisted)
+            socketInstance.off("join-room", JoinRoom)
+            socketInstance.off("room-list", RoomList)
+            socketInstance.off("connect", OnSocketConnect)
+            socketInstance.off("disconnect", OnSocketDisconnect)
+        }
+    }, [])
     useEffect(() => { myInfoRef.current = myInfo }, [myInfo])
     /* -------- PROVIDER -------- */
     const defaultValues: GlobalProps = {
-        socket, setsocket,
-        peer, setpeer,
+        socket,
+        peer,
         myInfo, setmyInfo,
-        IPv4, setIPv4,
         meetingCode, setmeetingCode,
         systemPopup, setsystemPopup,
         roomList,
--- client/types/globals.types.d.ts
+++ client/types/globals.types.d.ts
@@ -3,21 +3,18 @@ import { Peer } from "peerjs"
 import { Dispatch, SetStateAction } from "react"
 import { RoomProps, UserProps } from "./session.types";
 import { SystemPopupProps } from "./system.popup.types";
-interface GlobalProps {
-    socket: Socket | null
-    peer: Peer | null
+interface GlobalProps
+{
+    socket: Socket
+    peer: Peer
     myInfo: UserProps
-    IPv4: string
     meetingCode: string
     systemPopup: SystemPopupProps | null
     roomList: RoomProps[]
     rawWhitelist: string,
     whitelist: string[],
     /* -------------------------- */
-    setsocket: Dispatch<SetStateAction<Socket | null>>
-    setpeer: Dispatch<SetStateAction<Peer | null>>
     setmyInfo: Dispatch<SetStateAction<UserProps>>
-    setIPv4: Dispatch<SetStateAction<string>>
     setmeetingCode: Dispatch<SetStateAction<string>>
     setsystemPopup: Dispatch<SetStateAction<SystemPopupProps | null>>
     setRawWhitelist: Dispatch<SetStateAction<string>>,
