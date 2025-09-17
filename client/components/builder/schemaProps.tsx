052581c4e2f75afe0a5b6f626fa7d748d9a833a3
clarorecto54 <clarorecto54@gmail.com>
2025-09-17 22:57:11 +0800

    CLIENT : Collapsable schema properties

--- client/components/builder/schemaProps.tsx
+++ client/components/builder/schemaProps.tsx
@@ -1,81 +1,96 @@
-import { CardContent, TextField, CardHeader, Card, Avatar } from "@mui/material";
+import { CardContent, TextField, CardHeader, Card, Avatar, Box, IconButton, Collapse } from "@mui/material";
 import { useSchema } from "../hooks/useSchema";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import Image from "next/image";
 import { HtmlElement } from "@/types/Exam.types";
 
 export default function SchemaProperties()
 {
     const { schemaData, setSchemaData } = useSchema()
+    const [expanded, setExpanded] = useState<boolean>(true)
     function HandleTextField(element: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>)
     {
         const { name, value } = element.target
         setSchemaData(prev => ({ ...prev, [name]: name === "timeLimit" ? Number(value) : value }))
     }
-    return <Card>
-        <CardHeader
-            title="Exam Properties"
-            avatar={<Avatar
-                sx={{
-                    height: "24px", width: "24px", background: "none",
-                }}>
-                <Image
-                    alt=""
-                    src={require("@/public/images/Settings.svg")}
+    return <Box>
+        <Card className="Unselectable">
+            <Box onClick={() => setExpanded(!expanded)} sx={{ cursor: "pointer" }}>
+                <CardHeader
+                    className="transition-all duration-300"
+                    title="Exam Properties"
+                    avatar={<Avatar
+                        sx={{
+                            height: "24px", width: "24px", background: "none",
+                        }}>
+                        <Image
+                            alt=""
+                            src={require("@/public/images/Settings.svg")}
+                        />
+                    </Avatar>}
+                    action={<IconButton onClick={() => setExpanded(!expanded)}>
+                        <Image
+                            src={require("@/public/images/Arrow.svg")}
+                            alt="back"
+                            className={`h-[16px] w-[16px] rotate-${expanded ? 180 : 0} transition-all duration-300`}
+                        />
+                    </IconButton>}
+                    slotProps={{
+                        title: {
+                            fontFamily: "Montserrat, sans-serif",
+                            fontSize: "12pt",
+                            fontWeight: 500
+                        }
+                    }}
+                    sx={{ paddingBottom: expanded ? 0 : "16px" }}
                 />
-            </Avatar>}
-            slotProps={{
-                title: {
-                    fontFamily: "Montserrat, sans-serif",
-                    fontSize: "12pt",
-                    fontWeight: 500
-                }
-            }}
-            sx={{ paddingBottom: "0" }}
-        />
-        <CardContent className="flex flex-col gap-[16px] px-[16px]">
-            <TextField
-                variant="outlined"
-                size="small"
-                name="title"
-                label="Exam Name"
-                value={schemaData.title}
-                onChange={HandleTextField}
-                fullWidth
-                required
-                slotProps={{ htmlInput: { maxLength: 64 } }}
-            />
-            <TextField
-                variant="outlined"
-                size="small"
-                name="startSurveyText"
-                label="Survey Starting Text"
-                value={(schemaData.pages[0].elements[0] as HtmlElement).html}
-                onChange={(element) =>
-                {
-                    setSchemaData(prev =>
-                    {
-                        const updated = { ...prev }
-                        const startingDisplay = updated.pages[0].elements[0] as HtmlElement
-                        startingDisplay.html = element.target.value
-                        return updated
-                    })
-                }}
-                fullWidth
-                required
-                slotProps={{ htmlInput: { maxLength: 128 } }}
-            />
-            <TextField
-                variant="outlined"
-                size="small"
-                name="timeLimit"
-                label="Time Limit of Exam (in seconds)"
-                type="number"
-                value={schemaData.timeLimit}
-                onChange={HandleTextField}
-                fullWidth
-                required
-            />
-        </CardContent>
-    </Card>
+            </Box>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent className="flex flex-col gap-[16px] px-[16px]">
+                    <TextField
+                        variant="outlined"
+                        size="small"
+                        name="title"
+                        label="Exam Name"
+                        value={schemaData.title}
+                        onChange={HandleTextField}
+                        fullWidth
+                        required
+                        slotProps={{ htmlInput: { maxLength: 64 } }}
+                    />
+                    <TextField
+                        variant="outlined"
+                        size="small"
+                        name="startSurveyText"
+                        label="Survey Starting Text"
+                        value={(schemaData.pages[0].elements[0] as HtmlElement).html}
+                        onChange={(element) =>
+                        {
+                            setSchemaData(prev =>
+                            {
+                                const updated = { ...prev }
+                                const startingDisplay = updated.pages[0].elements[0] as HtmlElement
+                                startingDisplay.html = element.target.value
+                                return updated
+                            })
+                        }}
+                        fullWidth
+                        required
+                        slotProps={{ htmlInput: { maxLength: 128 } }}
+                    />
+                    <TextField
+                        variant="outlined"
+                        size="small"
+                        name="timeLimit"
+                        label="Time Limit of Exam (in seconds)"
+                        type="number"
+                        value={schemaData.timeLimit}
+                        onChange={HandleTextField}
+                        fullWidth
+                        required
+                    />
+                </CardContent>
+            </Collapse>
+        </Card>
+    </Box>
 }
