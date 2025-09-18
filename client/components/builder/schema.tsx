e5654283afe5c1ba3761c0e03af666031feca8e8
clarorecto54 <clarorecto54@gmail.com>
2025-09-18 23:41:07 +0800

    CLIENT : Make Schema Properties sticky

--- client/components/builder/schema.tsx
+++ client/components/builder/schema.tsx
@@ -7,10 +7,10 @@ import { useSchema } from "../hooks/useSchema"
 export default function SchemaBuilder()
 {
     const { schemaData } = useSchema()
-    return <Box className="pt-[16px] w-full" display="flex" flexDirection="column" gap="16px" overflow="hidden" height="100%">
+    return <Box className="pt-[16px] w-full" display="flex" flexDirection="column" gap="16px" overflow="hidden" height="100%" px={"8px"}>
         <SchemaHeader />
+        <SchemaProperties />
         <Box display="flex" flexDirection="column" gap="16px" overflow="auto" height="100%" paddingX="8px">
-            <SchemaProperties />
             {(schemaData.pages.length - 1) > 0 && schemaData.pages.slice(1).map((_, index) => <SchemaQuestions key={index} index={index + 1} />)}
         </Box>
     </Box>
