2c26f5e8b835739b6c482a31e5c989c27c71b69e
clarorecto54 <clarorecto54@gmail.com>
2025-09-19 23:42:52 +0800

    CLIENT : Cleanup Schema Data Listener

--- client/components/utils/schemaData.listener.ts
+++ client/components/utils/schemaData.listener.ts
@@ -3,12 +3,10 @@ import { SchemaErrors, QuestionErrors, SchemaDataListenerTypes, SchemaErrorListe
 
 export const SchemaDataListener: SchemaDataListenerTypes = (setSchemaData, schemaData, schemaKey, setSchemaErrors) =>
 {
-    console.clear()
     const instanceSchemaData = schemaData
     instanceSchemaData.timeLimit = instanceSchemaData.timeLimitPerPage * (instanceSchemaData.pages.length - 1)
     SchemaErrorListener(schemaData, schemaKey, setSchemaErrors)
     setSchemaData(instanceSchemaData)
-    console.log(JSON.stringify(instanceSchemaData, null, 2))
 }
 
 const SchemaErrorListener: SchemaErrorListenerTypes = (schemaData, schemaKey, setSchemaErrors) =>
