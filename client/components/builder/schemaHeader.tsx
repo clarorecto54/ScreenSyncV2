db25c396d0a27b5aa16a02895a728fdf83c9c3f1
clarorecto54 <clarorecto54@gmail.com>
2025-09-19 22:02:52 +0800

    CLIENT : Fix schema header

--- client/components/builder/schemaHeader.tsx
+++ client/components/builder/schemaHeader.tsx
@@ -1,38 +1,54 @@
-import { Box, Button, Typography } from "@mui/material"
+import { Button, CardHeader } from "@mui/material"
 import { useSchema } from "../hooks/useSchema"
 import Image from "next/image"
 
 export default function SchemaHeader()
 {
     const { setBuilderMode, builderPage } = useSchema()
-    return <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between">
-        <Button
-            variant="text"
-            size="small"
-            onClick={() => setBuilderMode(false)}
-            startIcon={
-                <Image
-                    src={require("@/public/images/Arrow.svg")}
-                    alt="back"
-                    className="h-[8px] w-[8px] rotate-90"
-                />
+    return <CardHeader
+        avatar={
+            <Button
+                size="small"
+                variant="contained"
+                onClick={() => setBuilderMode(false)}
+            >
+                Back
+            </Button>
+        }
+        title={builderPage}
+        slotProps={{
+            avatar: {
+                style: {
+                    zIndex: 1,
+                    margin: 0,
+                }
+            },
+            action: {
+                style: {
+                    zIndex: 1,
+                    margin: 0,
+                }
+            },
+            root: {
+                style: {
+                    position: "relative",
+                    margin: 0,
+                    padding: 0,
+                    paddingBottom: "8px",
+                }
             }
-            sx={{ fontFamily: "Montserrat, sans-serif", fontSize: "10pt" }}>
-            Back
-        </Button>
-        <Typography
-            sx={{
+        }}
+        sx={{
+            "& *": { margin: 0 },
+            "& .MuiCardHeader-content": {
+                width: "100%",
                 position: "absolute",
-                left: "50%",
-                transform: "translateX(-50%)",
-                fontFamily: "Montserrat, sans-serif",
-                fontWeight: 600,
+            },
+            "& .MuiCardHeader-title": {
                 fontSize: "16pt",
-            }}>
-            {builderPage}
-        </Typography>
-    </Box>
+                textAlign: "center",
+                fontWeight: 600,
+            }
+        }}
+    />
 }
