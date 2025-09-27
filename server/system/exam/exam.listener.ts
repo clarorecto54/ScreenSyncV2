d89e5b80fe1e7ca7fa27be8f5e88a189e9d931ec
clarorecto54 <clarorecto54@gmail.com>
2025-09-27 22:47:35 +0800

    SERVER : Schema Controllers

---
+++ server/system/exam/exam.listener.ts
@@ -0,0 +1,78 @@
+import { Socket } from "socket.io";
+import { ExamProp, ExamSocketMapping, SchemaSavefile } from "./exam.types";
+import { Decrypt, Encrypt } from "../../utils/crypto";
+import { readFileSync, writeFileSync } from "fs-extra";
+import { ServerLog } from "../log";
+
+export default function ExamSocketListener(socket: Socket<ExamSocketMapping>)
+{
+    const filename = "Exam-Templates.json"
+    const path = `../${filename}`
+    let saveList: SchemaSavefile[] = []
+    try
+    {
+        ServerLog("server", `Loading all schema...`)
+        const raw = readFileSync(path, "utf-8")
+        saveList = JSON.parse(raw) as SchemaSavefile[]
+        ServerLog("server", `All schema has been loaded`)
+    } catch
+    {
+        ServerLog("server", `${filename} has not been found, creating a new one...`)
+        writeFileSync(
+            path,
+            JSON.stringify(saveList, null, 1),
+            "utf-8"
+        )
+        ServerLog("server", `${filename} has been created`)
+    }
+    socket.on("SaveSchema", (schema) =>
+    {
+        const existing = saveList.some(save => save.id === schema.id)
+        if (existing) saveList = saveList.filter(save => save.id !== schema.id)
+        const savefile: SchemaSavefile = {
+            id: schema.id,
+            name: schema.title,
+            subject: schema.description,
+            date: new Date(),
+            data: Encrypt(schema)
+        }
+        saveList.push(savefile)
+        UpdateSavefile(path, saveList)
+    })
+    socket.on("DeleteSchema", (schema) =>
+    {
+        const existing = saveList.some(save => save.id === schema.id)
+        if (!existing) return
+        saveList = saveList.filter(save => save.id !== schema.id)
+        UpdateSavefile(path, saveList)
+    })
+    socket.on("GetSchema", (sendData) =>
+    {
+        ServerLog("socket", `${socket.id} is requesting for the list of schema...`)
+        const list: ExamProp[] = []
+        for (const save of saveList)
+            list.push(Decrypt(save.data))
+        sendData(list)
+        ServerLog("socket", `${socket.id} has recevied the updated list of schema`)
+    })
+}
+
+function ReadSavefile(path: string): SchemaSavefile[]
+{
+    ServerLog("server", `Loading all schema...`)
+    const raw = readFileSync(path, "utf-8")
+    const updatedList = JSON.parse(raw) as SchemaSavefile[]
+    ServerLog("server", `All schema has been loaded`)
+    return updatedList
+}
+
+function UpdateSavefile(path: string, saveList: SchemaSavefile[])
+{
+    ServerLog("server", `Saving schema...`)
+    writeFileSync(
+        path,
+        JSON.stringify(saveList, null, 1),
+        "utf-8"
+    )
+    ServerLog("server", `Schema has been saved`)
+}
--- server/utils/crypto.ts
+++ server/utils/crypto.ts
@@ -1,10 +1,12 @@
-export function Encrypt(data: unknown)
+import { ExamProp } from "../system/exam/exam.types"
+
+export function Encrypt(data: ExamProp): string
 {
     const text = JSON.stringify(data)
     return Buffer.from(text, "utf-8").toString("base64")
 }
 
-export function Decrypt(text: string)
+export function Decrypt(text: string): ExamProp
 {
     const data = Buffer.from(text, "base64").toString("utf-8")
     return JSON.parse(data)
