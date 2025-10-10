157ce517fa791eb1d33f4a1517d8261b89c5ca50
clarorecto54 <clarorecto54@gmail.com>
2025-10-11 06:49:49 +0800

    CLIENT : FIX missing fonts

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        custom: ["Montserrat", "sans-serif"]
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
