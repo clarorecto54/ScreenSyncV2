960271fae70108d248adf4bc291d7ac89e2a50c0
clarorecto54 <clarorecto54@gmail.com>
2025-10-25 01:40:13 +0800

    CLIENT : FIX bug on manual stop

--- client/components/hooks/useSchema.tsx
+++ client/components/hooks/useSchema.tsx
@@ -58,7 +58,7 @@ export function SchemaContextProvider({ children }: { children: ReactNode })
                         points++
                 mappedSocket.emit("SendResult", meetingCode, {
                     id: mappedSocket.id!,
-                    examId: schemaData.id,
+                    examId: instanceSchemaData.id,
                     name: myInfo.name,
                     score: points,
                     examName: instanceSchemaData.title,
